0be6bd06d5d154659410c17d3a1a9af4d2196f3d
jordyhers <jordy925@gmail.com>
2022-06-08 17:46:45 +0300

    Lesson_1_Comments

---
+++ Dimash/comment.py
@@ -0,0 +1,12 @@
+
+# Komut 
+"""
+My name is Jordy. I live in Almaty, I am 12 years old, I love football and
+I play every day.
+
+"""
+print('My name is Jordy. I live in Almaty, I am 12 years old, I love football and I play every day.')
+
+
+
+
