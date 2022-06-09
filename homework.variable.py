0cd3aaceebd3ee8b75e43d6baa2d13f040b12216
Dimash <106987002+OmarDimash@users.noreply.github.com>
2022-06-09 21:39:17 +0600

    Add files via upload

---
+++ homework.variable.py
@@ -0,0 +1,39 @@
+name,surname,birth_place, = "Anjel","Jemesson","USA",
+student1 ="Name: " + name,'Surname: ' + surname,'Birth_place: ' + birth_place,
+class1 = 98.7
+class2 = 67.9 
+class3 = 73.9 
+class4 = 100
+class_total = class1+class2+class3+class4
+total = class_total/4
+print("Name: " + name,'Surname: ' + surname,'Birth_place: ' + birth_place, total)
+
+name,surname,birth_place = "Sanzar","ferozan","Iran"
+student2 = 'Name: ' + name,'Surname: ' + surname,'Birth_place; ' + birth_place
+class_total2 = class3+class2+class3+class2
+total2 = class_total2/4
+print('Name: ' + name,'Surname: ' + surname,'Birth_place; ' + birth_place,total2)
+
+
+name,surname,birth_place = "Damir","Abdrahin","Almaty"
+student3 = 'Name:' + name,'Surname: ' + surname,'Birth_place: ' + birth_place
+class_total3 = class1+class4+class1+class4
+total3 = class_total3/4
+print('Name:' + name,'Surname: ' + surname,'Birth_place: ' + birth_place,total3)
+
+
+name,surname,birth_place = 'Dimash','Omar','Almaty'
+student4 = 'Name: ' + name,'Surname: ' + surname,'Birth_place; ' + birth_place
+class_total4=class4+class4+class4+class4
+total4 =class_total4/4
+print( 'Name: ' + name,'Surname: ' + surname,'Birth_place; ' + birth_place,total4)
+
+
+
+
+
+
+
+
+
+
