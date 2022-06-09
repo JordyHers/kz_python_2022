f2ab6ac0c78a7fbf51b33347f288612533e6f608
Ainurka <106985892+AinurAbil@users.noreply.github.com>
2022-06-09 21:54:46 +0600

    Add files via upload

---
+++ Ainur/odev.py
@@ -0,0 +1,63 @@
+#Studen1
+name = 'Olga'
+surname = 'Petrova'
+birth_year = str(2002)
+place_of_residence  = 'Moskva'
+
+grade1 = 96.3
+grade2 = 67.9
+grade3 = 73.9
+grade4 = 100
+
+grade_total = grade1+grade2+grade3+grade4
+total = grade_total / 4 
+
+print('Name: ' + name, 'Surname: ' + surname, 'Birth','year: ' + birth_year, 'Place','of','residence: ' + place_of_residence, total)
+
+#Student2
+name = 'Helly'
+surname = 'Poland'
+birth_year = str(2002)
+place_of_residence  = 'Bulgaria'
+
+grade1 = 88.9
+grade2 = 77.3
+grade3 = 82.5
+grade4 = 93.7
+
+grade_total = grade1+grade2+grade3+grade4
+total = grade_total / 4 
+print('Name: ' + name, 'Surname: ' + surname, 'Birth','year: ' + birth_year, 'Place','of''residence: ' + place_of_residence, total)
+
+
+#Student3
+name = 'Henry'
+surname = 'Holland'
+birth_year = str(1998)
+place_of_residence  = 'Luxemburg'
+
+grade1 = 100
+grade2 = 95.3
+grade3 = 92.8
+grade4 = 98.9
+
+grade_total = grade1+grade2+grade3+grade4
+total = grade_total / 4 
+
+print('Name: ' + name, 'Surname: ' + surname, 'Birth','year: ' + birth_year, 'Place','of','residence: ' + place_of_residence, total)
+
+#Student4
+name = 'Zuzi'
+surname = 'Lofin'
+birth_year = str(2003)
+place_of_residence  = 'Paris'
+
+grade1 = 97.9
+grade2 = 87.0
+grade3 = 95.5
+grade4 = 99.3
+
+grade_total = grade1+grade2+grade3+grade4
+total = grade_total / 4 
+
+print('Name: ' + name, 'Surname: ' + surname, 'Birth','year: ' + birth_year, 'Place','of','residence: ' + place_of_residence, total)
