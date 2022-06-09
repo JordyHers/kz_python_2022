d40acc8e9430b1917cbae45014c583ebbd0c3094
jordyhers <jordy925@gmail.com>
2022-06-09 21:13:33 +0300

    datatype added

---
+++ All_Python Lessons/datatype.py
@@ -0,0 +1,146 @@
+
+"""
+Variables
+string = 'Jordy'
+integer = 24 - 56 - 0 
+float = 3.7844563 
+
+
+list = ['banana','bread','washing','milk']
+tuple = ('banana','bread','washing','milk')
+bool = true, false 
+dict = 
+"""
+
+#Jordy
+name = 'Jordy'
+age = 35
+country = 'Turkey'
+salary = '5000 USD'
+job = 'Programmer'
+
+
+jordy = { "name " : "Jordy" , "age" : 35 , "country": "Turkey"}
+name, age ,country , salary , job = 'Jordy', 12, 'Turkey', '5000USD','Student'
+
+#Dimash
+name = 'Dimash'
+age = 12
+country = 'Kazakhstan'
+salary = '8000 USD'
+job = 'Student'
+
+
+# print(jordy)
+# print(dimash)
+
+#
+mercedes = {'color': 'black', 'model': 'A Class-Benz', 'speed': 230, 'price': 165000, 'currency': 'USD' }
+
+tesla = {'color': 'blue', 'model': 'S', 'speed': 330, 'price': 1165000, 'currency': 'TL' }
+
+# To display the specific value that we want
+# print(mercedes['color'])
+# print(str(tesla['speed']) + ' KM/H')
+
+
+
+number = 5j
+#print(type(number))
+# boolean ==> bool
+# 1. Mathematic True veya False
+#print(18 < 45)
+#print(3.0016 > 3.016)
+
+# 2. Autobus
+
+door1 = False
+door2 = False
+#print(door1)
+door1 = True
+#print(door1)
+
+
+"""
+STRINGS
+1. Multiline
+2. Letter position [0] [1]....
+3. Word length
+"""
+
+selam = 'hello'
+selam = "hello"
+
+text1 = 'hzdghfghgzdfjkghzfgzljkhgfkzghfkgzkhlgfkhzghfgzhkfghzgfhgkzhfgfzhjfzgffjzhghfzgfhgzfzklggzfgfhkzgfghzgfjgzhjfzghjgfzghkfklhgjhshghjgksfhgxjzgfghkszhfgjzgfghsgfgsjkghhglzgfgfgjkzfghzgfkghfgzjhgfhzgfaksgfhgafhkgakgfhjagfjhagjgkjfhafgafhgfjkq kcjvjkghskghlksjgljkgejkghe'
+
+
+
+# print(text1)
+# print()
+# print(text2)
+
+
+
+jordy = { 'name' : 'Jordy'}
+#print(jordy['name'])
+
+name = 'Dimashin'
+#print(name[0])
+
+car = 'Mercedes'
+#print(car[0],car[1],car[2],car[3],car[4],car[5])
+
+text2 = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+ainur do eiusmod tempor incididunt
+ut labore et dimash magna aliqua.
+Benim canim seni cok seviyorum,
+cunku cok guzelsin
+"""
+# len() uzunlugu bilmek icin
+#print(len(text2))
+
+# To find out if ainur is in the text2
+#print('ainur' and 'dimash' in text2)
+
+#print('aruzhan' in text2) #False
+#print('aruzhan' not in text2) #True
+
+
+
+
+
+text = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur interdum finibus justo. In augue nunc, rutrum vel porttitor vitae, varius ac nisi. Nunc euismod lectus sit amet eleifend elementum. Fusce sit amet dignissim est, ut auctor est. Integer ac velit tortor. Praesent auctor lorem volutpat justo placerat venenatis. Quisque efficitur dapibus nisl id convallis. Nulla volutpat, purus et facilisis faucibus, est risus porttitor nunc, a ornare sem sem vitae dui. Vestibulum ac risus mi. Integer dignissim ipsum nisl, at imperdiet lacus convallis quis. Mauris sed ligula eu diam volutpat auctor. Morbi pellentesque fringilla justo a sollicitudin. Maecenas aliquam tempor ligula a pellentesque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam in tortor risus.
+
+Sed eget arcu sed nibh pretium viverra sed id elit. Nullam vitae consequat dolor, id semper nulla. Nam magna ipsum, posuere et convallis vitae, ultrices in erat. Suspendisse placerat arcu vitae ante gravida auctor. Suspendisse eget tortor et ligula lobortis tristique quis ornare libero. Duis maximus a sem in egestas. Proin ac lacus ultrices, imperdiet tellus sed, fringilla risus. Aenean bibendum rhoncus pretium. Proin pretium risus vitae vehicula hendrerit. Nam maximus mollis massa, eget tempor nunc faucibus hendrerit. Integer arcu leo, varius ac aliquam sed, sodales luctus elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla sed elit a purus mollis sagittis. Curabitur ornare magna non erat fringilla, vel maximus libero feugiat. Aenean eu risus a lectus cursus accumsan ac eu orci. Nunc vel neque auctor risus accumsan dignissim.
+
+Nulla facilisi. Nulla non mi eget elit luctus molestie nec quis libero. Vivamus et dignissim nulla. Vivamus nulla est, bibendum nec nulla ut, convallis finibus enim. In gravida pellentesque augue facilisis gravida. Nunc ultrices arcu lorem, quis lacinia libero tempus ac. Donec sagittis nunc bibendum erat aliquam blandit. Sed egestas rutrum lectus non fermentum. Curabitur turpis arcu, malesuada vitae massa sed, dignissim scelerisque libero. Proin vel velit id ante mollis maximus eget eu dui. Fusce non elit semper, porta dolor id, sollicitudin libero. Vestibulum viverra arcu ante. Cras laoreet justo sed vulputate volutpat. Phasellus lacinia eros placerat magna congue, non aliquet nisl posuere. Pellentesque quis arcu in quam accumsan tincidunt.
+
+In id lectus at tellus rhoncus rhoncus eu in lorem. Maecenas dignissim ex tortor, nec porttitor sapien tincidunt dictum. Ut ullamcorper risus et bibendum vulputate. Donec aliquam laoreet ante sed vulputate. Ut aliquet ligula quam, sed fringilla ligula dapibus ut. Nunc interdum porta pellentesque. Sed sed nunc arcu.
+
+Suspendisse in feugiat justo, in placerat dui. Sed ullamcorper consequat massa a lobortis. Pellentesque sagittis ullamcorper turpis, id suscipit arcu. Integer viverra mauris vitae lacus egestas elementum. Sed pulvinar vel odio quis volutpat. Pellentesque felis magna, aliquam sed porta sit amet, cursus ac lectus. Nunc finibus orci eu blandit facilisis. Suspendisse potenti. Nam felis massa, rutrum quis rhoncus quis, hendrerit eu justo. Mauris vulputate vulputate consectetur.
+"""
+
+
+"""
+
+UYGULAMA 1
+
+1. How many letters are there in the following text.
+2. Find if the following words are in the text.
+3. If the words are in the text tell us how many letters they have.
+
+1. Сколько букв в следующем тексте.
+2. Найдите, есть ли в тексте следующие слова.
+3. Если слова есть в тексте, скажите, сколько в них букв. use len()
+
+     molestie,sollicitudin, faucibus, teknika , elveto , ligula
+     
+     
+"""
+print('The length of the text is :'+ str(len(text)))
+
+print("molestie" in text, "sollicitudin" in text,  "faucibus" in text, "teknika" in text, "elveto" in text, "ligula" in text)
+
+print("molestie : " + str(len('molestie')))
