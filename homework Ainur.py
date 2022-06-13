cf27ac7517b9acf22c6dc093c222e2368414a72b
Ainurka <106985892+AinurAbil@users.noreply.github.com>
2022-06-13 20:21:29 +0600

    Add files via upload

---
+++ homework Ainur.py
@@ -0,0 +1,76 @@
+#Odev1
+"""
+name = 'Payton'
+age = 25
+country = 'USA'
+job = 'Doctor'
+
+print(" This is ", name, 'He is ',age , 'I am from', country, ' I am a ', job)
+"""
+
+
+#Odev2
+"""
+LevingtonHotel = {'location' : 'Antalya', 'price':'2000', 'confort':'Very confortable','vistitors_note':'★★★★','distance':300, 'number_of_rooms':54,'swimming_pool': True}
+
+AlmatyHotel = {'location' : 'Almaty', 'price':'5000', 'confort':'Not confortable','vistitors_note':'★★★','distance':100, 'number_of_rooms':14,'swimming_pool': False}
+
+MevlanaHotel = {'location' : 'Istanbul', 'price':'2400', 'confort':'Best confortable','vistitors_note':'★★★★★','distance':200, 'number_of_rooms':32,'swimming_pool': True}
+
+DoubletreeHotel = {'location' : 'Izmir', 'price':'1400', 'confort':'Very confortable','vistitors_note':'★','distance':700, 'number_of_rooms':27,'swimming_pool': True}  
+
+
+x,y,z,v = LevingtonHotel['swimming_pool'] , AlmatyHotel['swimming_pool'] , MevlanaHotel['swimming_pool'], DoubletreeHotel['swimming_pool']
+
+print('Is the a swimming pool in the Levington Hotel: ' + str(x) , '\n' 'Is the a swimming pool in the Almaty Hotel: '+ str(y), '\n' 'Is the a swimming pool in the Mevlana Hotel: ' + str(z), '\n' 'Is the a swimming pool in the Doubletree Hotel:' + str(v))
+
+Hotel1 = 'Levington' 
+Hotel2 = 'Almaty'
+Hotel3 = 'Mevlana'
+Hotel4 = 'Doubletree'
+
+
+word1 = (Hotel1[0],Hotel1[1],Hotel1[2],Hotel1[3],Hotel1[4],Hotel1[5],Hotel1[6],Hotel1[7],Hotel1[8])
+print(len(Hotel1), len(Hotel2), len(Hotel3), len(Hotel4))
+
+price = 2000+5000+2400+1400
+all_price = price/4
+print(all_price)
+"""
+
+#Odev3
+
+text = """
+Turned it up should no valley cousin he. Speaking numerous ask did horrible packages set. Ashamed herself has distant can studied mrs. Led therefore its middleton perpetual fulfilled provision frankness. Small he drawn after among every three no. All having but you edward genius though remark one.
+Yourself off its pleasant ecstatic now law. Ye their mirth seems of songs. Prospect out bed contempt separate. Her inquietude our shy yet sentiments collecting. Cottage fat beloved himself arrived old. Grave widow hours among him .no you led. Power had these met least nor young. Yet match drift wrong his our.
+We diminution preference thoroughly if. Joy deal pain view much her time. Led young gay would now state. Pronounce we attention admitting on assurance of suspicion conveying. That his west quit had met till. Of advantage he attending household at do perceived. Middleton in objection discovery as agreeable. Edward thrown dining so he my around to.
+Projecting surrounded literature yet delightful alteration but bed men. Open are from long why cold. If must snug by upon sang loud left. As me do preference entreaties compliment motionless ye literature. Day behaviour explained law remainder. Produce can cousins account you pasture. Peculiar delicate an pleasant provided do perceive.
+May musical arrival beloved luckily adapted him. Shyness mention married son she his started now. Rose if as past near were. To graceful he elegance oh moderate attended entrance pleasure. Vulgar saw fat sudden edward way played either. Thoughts smallest at or peculiar relation breeding produced an. At depart spirit on stairs. She the either are wisdom praise things she before. Be mother itself vanity favour do me of. Begin sex was power joy after had walls miles.
+Seen you eyes son show. Far two unaffected one alteration apartments celebrated but middletons interested. Described deficient applauded consisted my me do. Passed edward two talent effect seemed engage six. On ye great do child sorry lived. Proceed cottage far letters ashamed get clothes day. Stairs regret at if matter to. On as needed almost at basket remain. By improved sensible servants children striking in surprise.
+Meant balls it if up doubt small purse. Required his you put the outlived answered position. An pleasure exertion if believed provided to. All led out world these music while asked. Paid mind even sons does he door no. Attended overcame repeated it is perceive marianne in. In am think on style child of. Servants moreover in sensible he it ye possible.
+Ye on properly handsome returned throwing am no whatever. In without wishing he of picture no exposed talking minutes. Curiosity continual belonging offending so explained it exquisite. Do remember to followed yourself material mr recurred carriage. High drew west we no or at john. About or given on witty event. Or sociable up material bachelor bringing landlord confined. Busy so many in hung easy find well up. So of exquisite my an explained remainder. Dashwood denoting securing be on perceive my laughing so.
+Contented get distrusts certainty nay are frankness concealed ham. On unaffected resolution on considered of. No thought me husband or colonel forming effects. End sitting shewing who saw besides son musical adapted. Contrasted interested eat alteration pianoforte sympathize was. He families believed if no elegance interest surprise an. It abode wrong miles an so delay plate. She relation own put outlived may disposed.
+Am no an listening depending up believing. Enough around remove to barton agreed regret in or it. Advantage mr estimable be commanded provision. Year well shot deny shew come now had. Shall downs stand marry taken his for out. Do related mr account brandon an up. Wrong for never ready ham these witty him. Our compass see age uncivil matters weather forbade her minutes. Ready how but truth son new under.
+"""
+"""
+print(len(text))
+print('admitting'in text, 'projecting'in text, 'leverage'in text,  'apartements'in text,  'circumstances ' in text, 'physicality' in text, 'perpetual'in text, 'resolution'in text,'visitors'in text, 'savage'in text, 'literature'in text, 'himself'in text, 'carriage'in text, 'vascular'in text, 'ashamed'in text, 'prestigious'in text)
+
+q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h = (len('admitting'), len('projecting'), len('leverage'), len('apartements'),len('circumstances'),len('physicality'),len('perpetual'),len('resolution'),len('visitors'),len('savage'),len('literature'),len('himself'),len('carriage'),len('vascular'),len('ashamed'),len('prestigious'))
+
+print(q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h)
+
+tag = (q+3 , w-3, e-3, r-3, t-3, y-3, u+3, i+3, o-3, p-3, a+3, s+3, d+3, f-3, g+3, h-3)
+print(tag)
+
+true = (q+u+i+a+s+d+g)
+print('total true ' + str(true))
+
+false = (w+e+r+t+y+o+p+f+h)
+print('total false ' + str(false))
+
+result = (true * false)
+print('The final result is ' + str(result))
+"""
+
+
