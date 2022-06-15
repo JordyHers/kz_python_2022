3b9de22338e25c332cd4b7ec4a59c326adca6416
Jordy Hershel IG <49708438+JordyHers@users.noreply.github.com>
2022-06-15 19:04:08 +0300

    Add files via upload

---
+++ Homework/methods_assignment.py
@@ -0,0 +1,8 @@
+person1 = {'name':'Jordy','surname':'Hershel','height':'1.75m','weight':'75Kg','color':'black',
+    'job':'teacher','salary':'2500$','country':'Turkey','birth_date':'12.09.1987'
+}
+
+person2 = {'name':'  jordy  ','surname':'HERSHEL','height':1.75,'weight':75.0,'color':'dark','job':'ogretmen','salary':2500, 'country': 'Istanbul','birth_date':'12/09/1987'
+}
+
+print(person1 is person2)
