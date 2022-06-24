6bbeab0174871e9d621334122b540059182b3988
Dimash <106987002+OmarDimash@users.noreply.github.com>
2022-06-24 14:52:06 +0600

    Update Dimash.homework.Functions.py

--- Dimash/Dimash.homework.Functions.py
+++ Dimash/Dimash.homework.Functions.py
@@ -11,15 +11,15 @@ num3=6
 num4=4
 
 
-def modulus(num1,num2):
-    sum=(num2%num2)
+def modulus(num2,num1):
+    sum=(num2%num1)
 print('This modulus of {} '.format(num2%num1))
 
 def division(num1,num2):
-    sum=(num2//num2)
+    sum=(num1//num2)
 print('This Floor division of {} '.format(num1//num2))
 
-def Exponentiation(num2,sun1):
+def Exponentiation(num3,sun4):
     sum=(num3**num4)
 print('This Exponentiation of {}'.format(num3**num4))
 
@@ -29,5 +29,5 @@ print('This multiplication of {}'.format(num3*num4))
 
 def division(num3,sun4):
     sum=(num3**num4)
-print('This division of {}'.format(num2/num4))
+print('This division of {}'.format(num3/num4))
 
