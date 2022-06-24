b7306a0ec97fadcf524c1f31c91822a295e3926c
Dimash <106987002+OmarDimash@users.noreply.github.com>
2022-06-24 14:47:48 +0600

    Add files via upload

---
+++ Dimash/Dimash.homework.Functions.py
@@ -0,0 +1,33 @@
+"""
+Modulus %
+Floor division //
+Exponentiation 2**3 = 2 x 2 x 2
+"""
+
+#_____________
+num1 = 78
+num2 = 90
+num3=6
+num4=4
+
+
+def modulus(num1,num2):
+    sum=(num2%num2)
+print('This modulus of {} '.format(num2%num1))
+
+def division(num1,num2):
+    sum=(num2//num2)
+print('This Floor division of {} '.format(num1//num2))
+
+def Exponentiation(num2,sun1):
+    sum=(num3**num4)
+print('This Exponentiation of {}'.format(num3**num4))
+
+def multiplication(num3,sun4):
+    sum=(num3**num4)
+print('This multiplication of {}'.format(num3*num4))
+
+def division(num3,sun4):
+    sum=(num3**num4)
+print('This division of {}'.format(num2/num4))
+
