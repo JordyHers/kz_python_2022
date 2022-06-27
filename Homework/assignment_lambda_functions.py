09982094231f1d1a121df0912a3f558887c176ef
Jordy Hershel IG <49708438+JordyHers@users.noreply.github.com>
2022-06-27 19:27:59 +0300

    Create assignment_lambda_functions.py

---
+++ Homework/assignment_lambda_functions.py
@@ -0,0 +1,70 @@
+"""
+Odev
+
+UYGULAMA 1
+Bu Fonksionlarda birkac hata var onlari duzeltin
+"""
+#1.
+ def test(name = 'James',*surname=12,age = str('Ben')):
+    return print('My name is {0}{2} ,I am {1} years old,'.format(name,age,surname))
+  
+test('Dimash',[])
+
+#2.
+def sinif_ortalamasi(*notlar):
+    toplam == 0
+    for note in notlar:
+        toplam+=note
+    print return(toplam)[/4]
+
+sinif_ortalamasi(60,82,40,91)
+
+"""
+UYGULAMA 2
+Напишите программу на Python для создания лямбда-функции,
+которая добавляет 15 к заданному числу, переданному в качестве аргумента,
+а также создайте лямбда-функцию, которая умножает аргумент x на аргумент y и печатает результат.
+
+Write a Python program to create a lambda function that adds 15 to a given number 
+passed in as an argument, also create a lambda function that multiplies argument 
+x with argument y and print the result
+
+Пример вывода:
+25
+76
+"""
+
+#_____________________________________________
+"""
+UYGULAMA 3
+
+iPhone
+Напишите функцию, которая будет принимать 5 аргументов. Аргументами будут цвет, 
+память, модель, цена и батарея.
+Функция будет использовать значения по умолчанию для каждого из параметров, чтобы убедиться,
+что что-то отображается, когда пользователь их не ввел.
+
+Write a function that will take 5 arguments. The arguments will be color, memory, model, 
+price, and battery. The function will use default values for each of the parameters 
+to make sure to display something when the user has not entered them.
+
+"""
+
+#_______________________________________________
+"""
+
+Uygulama 4 
+Напишите 4 функции. Каждая лямбда-функция будет вычислять
+1. Площадь квадрата 
+2. Теорема Пифагора 
+3. Площадь прямоугольника.
+4. Площадь круга.
+
+Write 4 functions. Each lambda function will calculate 
+1. Area of a Square 2. Pythagorean theorem 3. Area of a Rectangle. 
+4. Area of the circle
+
+#pi = 3.14159
+"""
+
+
