f07dabf4b80b2b51f0610714ff3a3da81834c9cc
Jordy Hershel IG <49708438+JordyHers@users.noreply.github.com>
2022-06-29 19:23:02 +0300

    Create assignment_classes.py

---
+++ Homework/assignment_classes.py
@@ -0,0 +1,10 @@
+"""
+Создайте класс, который будет принимать в качестве аргументов высоту, ширину, глубину и число Пи. Имя класса будет «Форма».
+Класс будет иметь 4 функции для вычисления площади, объема и периметра (окружности) треугольника, круга, прямоугольника, квадрата, куба и т. д.
+
+Create a class that will take as arguments height, width, depth, and Pi. The name of the class will be 'Shape'.
+The class will have 4 functions to calculate the area, the volume, and the perimeter(circumference) of a Triangle, 
+Circle, Rectangle, Square, Cube or etc...
+
+"""
+
