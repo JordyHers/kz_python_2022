68cd6cb8d76174feda0a092477504271068e03fd
Jordy Hershel IG <49708438+JordyHers@users.noreply.github.com>
2022-07-05 19:48:34 +0300

    Create assignment_inheritance_modules.py

---
+++ Homework/assignment_inheritance_modules.py
@@ -0,0 +1,27 @@
+"""
+
+Write a Python program that will have 2 python files.
+main.py
+models.py
+
+
+1. In the main.py you will only import the modules and call the functions. You will create 3 employees and one manager. Then you will have to print their info (name, surname, etc..). Finally will print if they can hire or fire anyone.
+
+2. In the models.py file you will create 3 classes. (Boss/Employee and Manager) The parent class will be Employee. The employee class will take name, surname, age, and position.
+The Manager will be a child class of Employee (inheritance). It will only have one 'def' function called canFireorHireEmployee(). This function will return true. Then the last class is a Boss class(inheritance Manager). It will have a single def function called CanFireManager() that you will set to return true.
+******************************************************************
+Напишите программу Python, которая будет иметь 2 файла Python.
+main.py
+модели.py
+
+
+1. В main.py вы будете только импортировать модули и вызывать функции. Вы создадите 3 сотрудников и одного менеджера. 
+Затем вам нужно будет распечатать их информацию (имя, фамилия и т. д.). Наконец напечатают, смогут ли они нанять или уволить кого-либо.
+
+2. В файле models.py вы создадите 3 класса. (Босс/Сотрудник и Менеджер) Родительским классом будет Сотрудник.
+Класс сотрудников примет имя,фамилию, возраст и должность.
+Менеджер будет дочерним классом Сотрудника (наследование). 
+У него будет только одна функция «def», называемая canFireorHireEmployee(). 
+Эта функция вернет true. Тогда последний класс - это класс Boss (менеджер наследования).
+У него будет единственная функция определения, называемая CanFireManager(), которую вы установите для возврата true.
+"""
