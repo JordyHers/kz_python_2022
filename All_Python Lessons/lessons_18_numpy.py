429b676571f25803c5630b7c1e8d33acac935e75
Jordy Hershel IG <49708438+JordyHers@users.noreply.github.com>
2022-07-18 19:33:12 +0300

    Create lessons_18_numpy.py

---
+++ All_Python Lessons/lessons_18_numpy.py
@@ -0,0 +1,13 @@
+"""
+Modules 
+math/json/re/urllib/camelcase;
+NUMPY
+"""
+
+import numpy as np
+#1. Version sorabiliriz
+print('numpy version {}'.format(np.__version__))
+
+#2. kolayca array olusturmak
+np_array = a = np.array([1,2,3,5])
+print(a)
