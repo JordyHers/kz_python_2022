c4803da867b11aab9a95f6c93d3cd9561db7e67e
jordyhers <jordy925@gmail.com>
2022-07-20 19:14:19 +0300

    lesson19

--- All_Python Lessons/lessons_19_pandas.py
+++ All_Python Lessons/lessons_19_pandas.py
@@ -1,1 +1,51 @@
-pandas = 'pandas'
+"""
+PANDAS
+CSV file
+"""
+import pandas as pd
+import numpy as np
+import config 
+import sys
+import os
+
+path= os.path.dirname(sys.argv[0])
+output= os.path.join(path,config.__package__)
+print("pandas version: {}".format(pd.__version__))
+
+# Pandas icin Series kelimesi kullaniyoruz (Sutun)
+data = pd.Series(np.random.randn(5))
+#print(data)
+
+
+#2. Pandas ile cok Series olusturabiliriz bunun adi DATAFRAME
+dictionary = {"Isim":['Laura','Gulnara','Ainur','Dimash','Abduvaris','Sanzhar'],"Arasinav":[89,93,98,98,76,50],"Final":[100,100,100,100,100,50]}
+df_data = pd.DataFrame(data = dictionary)
+
+
+#3. Indeks olusturmak
+i = 1 
+index=[]
+while(i<len(dictionary['Isim']) + 1):
+    index.append(i)
+    i+=1
+df_data = pd.DataFrame(data = dictionary, index = index)
+print(df_data)
+
+#4. Dataframe den CSV format kaydetmek
+"""
+        Isim  Arasinav  Final
+1      Laura        89    100
+2    Gulnara        93    100
+3      Ainur        98    100
+4     Dimash        98    100
+5  Abduvaris        76    100
+6    Sanzhar        50     50
+"""
+try:
+    df_data.to_csv(output,encoding='latin1')
+    print('Sizin dosyaniz kaydedildi.')
+    #print(output)
+except:
+    print(' ** Hata oldu dosya kaydedilmedi **')
+  
+   
