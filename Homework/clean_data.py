4e0b8796c8d1614427a3b1ecb7a854900485f9cd
Jordy Hershel IG <49708438+JordyHers@users.noreply.github.com>
2022-07-22 22:18:20 +0300

    Update clean_data.py

--- Homework/clean_data.py
+++ Homework/clean_data.py
@@ -9,8 +9,8 @@ import numpy as np
 
 # 1. Drop Duplicated Rows and Only keep unique ones.
 # 2. All Names Should Start with a [C]apital Letter  Alexender for example
-# 3. All First Names Should Be written in Capital Letters
-#   3.a  If name is null a name replace it by writing False
+# 3. All First Names Should Be written in Capital Letters [Dimash OMAR]
+#   3.a  If name is null replace it by writing False
 # 4. All SSN should be unique. can not be the same.
 #   4.a If SSN does not exist write 000-00-0000
 # 5. Remove all negative signs from the Test3 Column
@@ -26,10 +26,14 @@ import numpy as np
 
 # 1. Удалите повторяющиеся строки и оставьте только уникальные.
 # 2. Все имена должны начинаться с заглавной буквы, например, Alexender.
-# 3. Все имена должны быть написаны заглавными буквами
+# 3. Все имена должны быть написаны заглавными буквами [Димаш ОМАР]
 # 3.a Если имя равно null, замените его, написав False
 # 4. Все SSN должны быть уникальными. не может быть одинаковым.
 # 4.a Если SSN не существует, напишите 000-00-0000
 # 5. Удалите все отрицательные знаки из столбца Test3
 # 5.a Если тестовое значение равно Nan, замените на Test Column mean.
 # 6. Новый файл должен быть сохранен как CSV в папке пути
+
+
+
+
